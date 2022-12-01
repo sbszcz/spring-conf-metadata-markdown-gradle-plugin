33d6b9a086812548792384dd2357937393fdaba3
Sebastian Barszczewski <sebastian.barszczewski@gmail.com>
2022-12-01 09:36:49 +0100

    testing all the things

--- src/test/kotlin/dev/sbszcz/gradle/plugin/PluginFunctionalTest.kt
+++ src/test/kotlin/dev/sbszcz/gradle/plugin/PluginFunctionalTest.kt
@@ -2,12 +2,15 @@ package dev.sbszcz.gradle.plugin
 
 import assertk.assertThat
 import assertk.assertions.contains
+import assertk.assertions.exists
 import assertk.assertions.isEqualTo
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.gradle.testkit.runner.TaskOutcome.FAILED
 import org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import org.gradle.testkit.runner.TaskOutcome.UP_TO_DATE
 import org.gradle.testkit.runner.UnexpectedBuildFailure
+import org.intellij.lang.annotations.Language
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -19,6 +22,7 @@ import java.nio.file.attribute.PosixFilePermissions.fromString
 import kotlin.io.path.Path
 import kotlin.io.path.appendText
 import kotlin.io.path.createDirectories
+import kotlin.io.path.createDirectory
 import kotlin.io.path.createFile
 import kotlin.io.path.isDirectory
 import kotlin.io.path.readText
@@ -52,6 +56,33 @@ class PluginFunctionalTest {
     lateinit var buildFile: Path
     lateinit var subFolder1: Path
     lateinit var subFolder2: Path
+
+    @Language("json")
+    val SPRING_CONF_METADATA_SAMPLE_1 = """
+    {
+      "properties": [
+        {
+          "name": "foo",
+          "type": "java.lang.Boolean",
+          "description": "example",
+          "sourceType": "Main"
+        }
+      ]
+    }""".trimIndent()
+
+    @Language("json")
+    val SPRING_CONF_METADATA_SAMPLE_2 = """
+    {
+      "properties": [
+        {
+          "name": "bar",
+          "type": "java.lang.String",
+          "description": "example",
+          "sourceType": "Main",
+          "defaultValue": "bar"
+        }
+      ]
+    }""".trimIndent()
 
     @BeforeEach
     fun setup() {
@@ -90,30 +121,11 @@ class PluginFunctionalTest {
     @Test
     fun `renderMetadataTable task execution is successful for default README_md`() {
 
-        val readmeFile = projectDir
-            .resolve("README.md")
-            .createFile(asFileAttribute(fromString("rw-------")))
-            .addText("""
-                <!-- springconfmetadata -->
-                <!-- /springconfmetadata -->
-            """.trimIndent())
+        val readmeFile = createReadmeFileWithTags()
 
         subFolder1.resolve("spring-configuration-metadata.json")
             .createFile()
-            .addText(
-                """
-                  {
-                  "properties": [
-                    {
-                      "name": "foo",
-                      "type": "java.lang.Boolean",
-                      "description": "example",
-                      "sourceType": "Main"
-                    }
-                  ]
-                }   
-            """.trimIndent()
-        )
+            .addText(SPRING_CONF_METADATA_SAMPLE_1)
 
         val result = executeGradleWithArgs("renderMetadataTable")
 
@@ -133,50 +145,34 @@ class PluginFunctionalTest {
     }
 
     @Test
-    fun `renderMetadataTable should render a table for each detected spring-configuration-metadata_json file`() {
-
+    fun `renderMetadataTable task should do nothing if marker tags are in one line`() {
         val readmeFile = projectDir
             .resolve("README.md")
             .createFile(asFileAttribute(fromString("rw-------")))
-            .addText("""
-                <!-- springconfmetadata -->
-                <!-- /springconfmetadata -->
-            """.trimIndent())
+            .addText("<!-- springconfmetadata --><!-- /springconfmetadata -->")
 
         subFolder1.resolve("spring-configuration-metadata.json")
             .createFile()
-            .addText(
-                """
-                  {
-                  "properties": [
-                    {
-                      "name": "foo",
-                      "type": "java.lang.Boolean",
-                      "description": "example",
-                      "sourceType": "Main"
-                    }
-                  ]
-                }   
-            """.trimIndent()
-            )
+            .addText(SPRING_CONF_METADATA_SAMPLE_1)
+
+        val result = executeGradleWithArgs("renderMetadataTable")
+
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(SUCCESS)
+        assertThat(readmeFile.readText()).isEqualTo("<!-- springconfmetadata --><!-- /springconfmetadata -->\n")
+    }
+
+    @Test
+    fun `renderMetadataTable should render a table for each detected spring-configuration-metadata_json file`() {
+
+        val readmeFile = createReadmeFileWithTags()
+
+        subFolder1.resolve("spring-configuration-metadata.json")
+            .createFile()
+            .addText(SPRING_CONF_METADATA_SAMPLE_1)
 
         subFolder2.resolve("spring-configuration-metadata.json")
             .createFile()
-            .addText(
-                """
-                  {
-                  "properties": [
-                    {
-                      "name": "bar",
-                      "type": "java.lang.String",
-                      "description": "example",
-                      "sourceType": "Main",
-                      "defaultValue": "bar"
-                    }
-                  ]
-                }   
-            """.trimIndent()
-            )
+            .addText(SPRING_CONF_METADATA_SAMPLE_2)
 
         val result = executeGradleWithArgs("renderMetadataTable")
 
@@ -197,8 +193,143 @@ class PluginFunctionalTest {
             <!-- /springconfmetadata -->
         
         """.trimIndent())
+    }
+
+    @Test
+    fun `renderMetadataTable task should ignore spring-configuration-metadata_json in build folders`() {
+
+        val readmeFile = createReadmeFileWithTags()
+
+        subFolder1.resolve("spring-configuration-metadata.json")
+            .createFile()
+            .addText(SPRING_CONF_METADATA_SAMPLE_1)
+
+        val ignoredConfig = projectDir.resolve("build")
+            .createDirectory()
+            .resolve("spring-configuration-metadata.json")
+            .createFile()
+            .addText(SPRING_CONF_METADATA_SAMPLE_2)
+
+        assertThat(ignoredConfig.toFile()).exists()
+
+        val result = executeGradleWithArgs("renderMetadataTable")
+
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(SUCCESS)
+        assertThat(readmeFile.readText()).isEqualTo("""
+        <!-- springconfmetadata -->
+        **Source**: */test-project/subfolder_1/spring-configuration-metadata.json*
+
+        | Name | Type | Description | Default | Source |
+        |:---|:---|:---|:---|:---|
+        | foo | java.lang.Boolean | example | n/a | Main |
+
+        <!-- /springconfmetadata -->
+        
+        """.trimIndent())
 
     }
+
+    @Test
+    fun `renderMetadataTable should work correctly with gradle caching features`() {
+
+        val readmeFile = createReadmeFileWithTags()
+
+        val springConfMetadataFile = subFolder1.resolve("spring-configuration-metadata.json")
+            .createFile()
+            .addText(SPRING_CONF_METADATA_SAMPLE_1)
+
+        // initial task execution
+        var result = executeGradleWithArgs("renderMetadataTable")
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(SUCCESS)
+
+        // second task execution without input/output changes
+        result = executeGradleWithArgs("renderMetadataTable")
+
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(UP_TO_DATE)
+        assertThat(readmeFile.readText()).isEqualTo("""
+        <!-- springconfmetadata -->
+        **Source**: */test-project/subfolder_1/spring-configuration-metadata.json*
+
+        | Name | Type | Description | Default | Source |
+        |:---|:---|:---|:---|:---|
+        | foo | java.lang.Boolean | example | n/a | Main |
+
+        <!-- /springconfmetadata -->
+        
+        """.trimIndent())
+
+        // change task input file
+        springConfMetadataFile.addText("\n\n")
+
+        result = executeGradleWithArgs("renderMetadataTable")
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(SUCCESS)
+
+        // third task execution without input/output changes
+        result = executeGradleWithArgs("renderMetadataTable")
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(UP_TO_DATE)
+
+        // change task output
+        readmeFile.addText("\nA new line")
+        result = executeGradleWithArgs("renderMetadataTable")
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(SUCCESS)
+
+        assertThat(readmeFile.readText()).isEqualTo("""
+        <!-- springconfmetadata -->
+        **Source**: */test-project/subfolder_1/spring-configuration-metadata.json*
+
+        | Name | Type | Description | Default | Source |
+        |:---|:---|:---|:---|:---|
+        | foo | java.lang.Boolean | example | n/a | Main |
+
+        <!-- /springconfmetadata -->
+        
+        A new line
+        
+        """.trimIndent())
+
+        // fourth task execution without input/output changes
+        result = executeGradleWithArgs("renderMetadataTable")
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(UP_TO_DATE)
+
+        // change task input by adding a second spring-configuration-metadata.json file
+        subFolder2.resolve("spring-configuration-metadata.json")
+            .createFile()
+            .addText(SPRING_CONF_METADATA_SAMPLE_2)
+
+        result = executeGradleWithArgs("renderMetadataTable")
+        assertThat(result.task(":renderMetadataTable")?.outcome).isEqualTo(SUCCESS)
+
+        assertThat(readmeFile.readText()).isEqualTo("""
+            <!-- springconfmetadata -->
+            **Source**: */test-project/subfolder_1/spring-configuration-metadata.json*
+            
+            | Name | Type | Description | Default | Source |
+            |:---|:---|:---|:---|:---|
+            | foo | java.lang.Boolean | example | n/a | Main |
+            **Source**: */test-project/subfolder_2/spring-configuration-metadata.json*
+            
+            | Name | Type | Description | Default | Source |
+            |:---|:---|:---|:---|:---|
+            | bar | java.lang.String | example | bar | Main |
+            
+            <!-- /springconfmetadata -->
+            
+            A new line
+        
+        """.trimIndent())
+
+    }
+
+    private fun createReadmeFileWithTags() = projectDir
+        .resolve("README.md")
+        .createFile(asFileAttribute(fromString("rw-------")))
+        .addText(
+            """
+                <!-- springconfmetadata -->
+                <!-- /springconfmetadata -->
+            """.trimIndent()
+        )
+
 
     private fun executeGradleWithArgs(vararg arguments: String): BuildResult = GradleRunner.create()
         .withProjectDir(projectDir.toFile())
